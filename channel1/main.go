99bb2ec1736766d249c9f3f77fc2e4dbf09afdc8
Caio Melo <c.melo92@gmail.com>
2024-02-21 11:50:41 -0300

    Create two examples using channels

--- channel1/main.go
+++ channel1/main.go
@@ -8,18 +8,22 @@ func main() {
 	ch := make(chan int)
 	sum := 0
 
-	go func() {
-		for index := range 10 {
-			ch <- index
-			fmt.Printf("Sent %d to channel\n", index)
-		}
-		close(ch)
-	}()
+	for index := range 10 {
+		sumValue := index
+		go func() {
+			ch <- sumValue
+			fmt.Printf("Sent %d to channel\n", sumValue)
+		}()
+	}
 
 	for msg := range ch {
 		fmt.Printf("Received from the channel: %d\n", msg)
 		sum += msg
 		fmt.Printf("Total sum is now %d\n", sum)
+
+		if sum == 45 {
+			close(ch)
+		}
 	}
 	fmt.Printf("Sum is: %d", sum)
 }
